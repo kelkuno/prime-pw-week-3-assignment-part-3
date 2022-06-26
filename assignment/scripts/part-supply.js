21984597f69327e6799ffc85f43fbb8d22e8c170
Kelsey Kuno <kelsey.kuno@gmail.com>
2022-06-26 17:04:02 -0500

    all stretch questions answered.

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -46,14 +46,51 @@ console.log(supplyChanges);
 console.log('6. Showing supplyChanges...');
 
 
+
+for(i=0; i<supplyChanges.length; i++){
+    let x = supplyChanges[i];
+
+    if(x > 0){
+        console.log('Added ' + x + ' parts.');
+    } else if(x === 0) {
+        console.log('No change.');
+    } else {
+        console.log('Removed ' + x +' parts.')
+    }
+    
+}
+
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop. 
 console.log('7. Showing supplyChanges with "for of" loop');
 
+for(part of supplyChanges) {
+    x = part;
+
+    if(x > 0){
+        console.log('Added ' + x + ' parts.');
+    } else if(x === 0) {
+        console.log('No change.');
+    } else {
+        console.log('Removed ' + x +' parts.')
+    }
+
+}
+
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
 console.log('8. Total supplies available is:');
+
+let sum = 0;
+console.log(supplyChanges);
+
+for(i=0; i<supplyChanges.length; i++) {
+    sum = supplyChanges[0] + supplyChanges[1] + supplyChanges[2] + supplyChanges[3] + supplyChanges[4] + supplyChanges[5];
+    console.log(sum);
+}
+
+
 
 // 9. We have a large stash of parts in our warehouse that we 
 //    need to box up and get ready for shipment. 
@@ -62,3 +99,17 @@ console.log('8. Total supplies available is:');
 //    no more boxes can be filled.
 //    Then log how many boxes were filled, and how many parts are left over.
 console.log('9. Filling boxes with a "while" loop');
+
+let totalParts = 572;
+let boxSpace = 7;
+let boxes = 0;
+let rest = totalParts;
+
+while (rest >= boxSpace){
+    rest = rest - boxSpace;
+    boxes++;
+}
+console.log(boxes);
+console.log(rest);
+console.log(boxes + ' boxes have been filled and there are ' + rest + ' parts leftover.');
+
